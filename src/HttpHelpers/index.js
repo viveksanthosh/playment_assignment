a8614e243a48f91fe74b6ce508014e9f70531edc
Vivek <vivek.santhosh@inmobi.com>
2018-05-12 15:12:45 +0530

    added stubbed remove folder (async)

--- src/HttpHelpers/index.js
+++ src/HttpHelpers/index.js
@@ -1,17 +1,35 @@
 
- class HttpCall {
+class HttpCall {
     constructor() {
         this.folderTree = {}
     }
     addNewFolder(name, path) {
-        path = path.split('/').filter(p => (p !== '' && p !== '.'));
-        console.log("path ", path)
-        let subTree = path.length === 0 ? this.folderTree: path.reduce((result, node)=> result[node], this.folderTree);
-        console.log("subTree ", subTree)
-        subTree[name] = {}
+        return new Promise((resolve, reject) => {
+            try {
+                path = path.split('/').filter(p => (p !== '' && p !== '.'));
+                let subTree = path.length === 0 ? this.folderTree : path.reduce((result, node) => result[node], this.folderTree);
+                subTree[name] = {};
+                resolve();
+            } catch (e) {
+                console.warn('error, path does not exist')
+                reject();
+            }
+        })
     }
 
-    
+    getFolders(path){
+        return new Promise((resolve, reject) => {
+            try {
+                path = path.split('/').filter(p => (p !== '' && p !== '.'));
+                let subTree = path.length === 0 ? this.folderTree : path.reduce((result, node) => result[node], this.folderTree);
+                resolve(Object.keys(subTree));
+            } catch (e) {
+                console.warn('error, path does not exist')
+                reject();
+            }
+        })
+    }
+
 }
 
 let call = new HttpCall();
