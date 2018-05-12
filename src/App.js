e985a8e5251c86fbb8b1f7f9b5e032ff2491771c
Vivek <vivek.santhosh@inmobi.com>
2018-05-12 11:51:22 +0530

    css boilerplate for the app ready

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import SideBar from './atoms/Sidebar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <SideBar />
+        <div className="container">
+          <div className="element" ><p>3 </p></div>
+          <div className="element" ><p>3 </p></div>
+          <div className="element" ><p>3 </p></div>
+  
+        </div>
+      </Fragment>
     );
   }
 }
